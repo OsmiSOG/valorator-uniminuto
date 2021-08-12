2b0eec871a504ca70c9565427a65447754a2d3a4
root <santiago.ooo6@gmail.com>
2021-08-12 17:36:45 -0400

    agregando server api remote

--- valorator/src/config.js
+++ valorator/src/config.js
@@ -3,7 +3,7 @@ const debug = 'dev'
 const config = {
     accesTokenMapBox: 'sk.eyJ1Ijoib3NtaXNvZyIsImEiOiJja3MzbjRwengwdGJxMnVxcTRmczhkbzd0In0.WhF86Hzw6_YMRcwsFMQXBg',
     dev: 'http://localhost/valoratorUNIMINUTO/valoratorBackend/controller/index.php',
-    prod: 'http://ip/valoratorUNIMINUTO/valoratorBackend/controller/index.php'
+    prod: 'http://142.44.246.12:86/valoratorUNIMINUTO/valoratorBackend/controller/index.php'
 }
 
 export {debug, config}
